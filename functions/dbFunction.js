46142c0bcaedfeb707d71294e357c5c1bec4bfb6
Giiin <zerepgen@gmail.com>
2025-08-03 20:20:39 +0800

    Update dbFunction.js
    connected it to the clevercloud

--- functions/dbFunction.js
+++ functions/dbFunction.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'sample_db'
+  host: 'by5cdsyn2oc5fg3ab4yd-mysql.services.clever-cloud.com',
+  user: 'urzlhjuml3lmia5p',
+  password: 'ZXvVBuRICUjp21nq8OIg',
+  database: 'by5cdsyn2oc5fg3ab4yd'
 });
+
 
 module.exports = db;
