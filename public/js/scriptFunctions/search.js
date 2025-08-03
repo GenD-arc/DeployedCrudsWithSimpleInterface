5c81756d311786eb2aba904d35c30b1b77d1d783
Giiin <zerepgen@gmail.com>
2025-08-03 20:42:41 +0800

    Update search.js

--- public/js/scriptFunctions/search.js
+++ public/js/scriptFunctions/search.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://simplecrudstable.onrender.com/";
+const baseUrl = "http://simplecrudstable.onrender.com";
 
 export const searchAccount = async () => {
       const email = document.getElementById("searchInput").value;
@@ -30,3 +30,4 @@ export const searchAccount = async () => {
       alert("Search Results: " + JSON.stringify(data));
 
     }
+
